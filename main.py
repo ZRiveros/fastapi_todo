2e14d2426775ce2fe3b6765f5e8829e16b8f23f2
Zebastian <riverosrolon@hotmail.com>
2025-11-25 23:14:34 +0100

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -92,3 +92,29 @@ async def delete_item(item_id: str):
 # To add Todo items, you can use tools like Postman or curl to send POST requests to the /todo endpoint with a JSON body containing the title of the todo item.
 # CRUD operation works well in swagger UI at http://127.0.0.1:8000/docs which adds all Todo items to MongoDB database.
 # Make sure MongoDB server is running locally on default port 27017 before starting the FastAPI app.
+# Start the MongoDB server using the command: sudo systemctl start mongod
+# Check status - sudo systemctl status mongod
+# Start mongo shell - mongosh
+# To show all databases - show dbs
+# To use a specific database - use <Todo_db>
+# to see all collections in the database - show collections
+# Get all documents in a collection - db.todos.find().pretty()
+# Count all documents in a collection - db.todos.countDocuments()
+# To exit mongo shell - exit
+
+# Post - curl -X POST http://127.0.0.1:8000/todos \
+# -H "Content-Type: application/json" \
+# -d '{"title": "Lära mig FastAPI"}'
+
+# Get - curl http://127.0.0.1:8000/todos
+
+# Get by ID - curl http://127.0.0.1:8000/todos/<id>
+
+# curl -X PUT http://127.0.0.1:8000/todos/<id> \
+# -H "Content-Type: application/json" \
+# -d '{"title": "Nytt namn"}' 
+
+# Delete - curl -X DELETE http://127.0.0.1:8000/todos/<id>
+# Note: Replace <id> with the actual ID of the todo item you want to update or delete.
+
+# 
